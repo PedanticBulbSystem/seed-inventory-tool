fe6ce8c83e8f6094a075aa4b6f80fcb4f617dda0
PedanticBulbSystem <marygastil@yahoo.com>
2019-12-15 11:44:03 -0800

    updated view to handle bx463

--- sql/5_vw_date_open_n_items_for_graph.sql
+++ sql/5_vw_date_open_n_items_for_graph.sql
@@ -1,16 +1,13 @@
--- View: bx.vw_date_open_n_items_for_graph
-
- --DROP VIEW bx.vw_date_open_n_items_for_graph;
-
---CREATE OR REPLACE VIEW bx.vw_date_open_n_items_for_graph
- --AS
- SELECT d.bx_id,
+CREATE OR REPLACE VIEW bx.vw_date_open_n_items_for_graph
+ AS
+ SELECT substring(d.bx_id from 1 for 6)::character varying(7) as bx_id,
     d.date_open,
-    max(i.item) AS max_item, count(*) AS n_items
+    max(i.item) AS max_item,
+    count(*) AS n_items
    FROM bx.bx_dates d
      LEFT JOIN bx.bx_items i ON d.bx_id::text = i.bx_id::text
   WHERE d.bx_id::text !~~ 'SX%'::text
-  GROUP BY d.date_open, d.bx_id
-  having max(i.item) <> count(*) OR max(i.item) is null
-  ORDER BY d.bx_id;
+  GROUP BY d.date_open, substring(d.bx_id from 1 for 6)
+  ORDER BY substring(d.bx_id from 1 for 6);
+
 
